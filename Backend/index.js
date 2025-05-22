63d8aea4ebe837ec95c8a61d062eac2e617c94da
Ali Gates <128673394+AliGates915@users.noreply.github.com>
2025-05-22 16:23:44 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://ag-tech-ii-project.vercel.app/'
+      'https://ag-tech-ii-project.vercel.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
